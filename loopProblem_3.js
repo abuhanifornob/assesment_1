4f99a07f76d3e2313f38f8e84dee254771120d37
Abu Hanif Arnob <hanifcse90@gmail.com>
2022-08-03 05:52:33 +0600

    Loop Problem 3 & 4 add

---
+++ loopProblem_3.js
@@ -0,0 +1,5 @@
+// Display all enven numbers form 412 to 456.
+
+for (var i = 412; i <= 456; i += 2) {
+    console.log(i);
+}
---
+++ loopProblem_4.js
@@ -0,0 +1,5 @@
+// .disply all odd numbers form 581 to 623.
+
+for (var i = 581; i <= 623; i += 2) {
+    console.log(i)
+}
