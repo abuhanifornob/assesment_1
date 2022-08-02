0cedf87677c336c36a3989970cd354ac2d12c3a3
Abu Hanif Arnob <hanifcse90@gmail.com>
2022-08-03 05:46:49 +0600

    loopProblem 8 add

---
+++ loopProblem_8.js
@@ -0,0 +1,8 @@
+// .crate an array for the mobile phones . display all the element of the array by using a while loop.
+
+var phone = ['nokya', 'samsung', 'MI', 'lenevo', 'walton'];
+var i = 0;
+while (i < phone.length) {
+    console.log(phone[i]);
+    i += 1;
+}
